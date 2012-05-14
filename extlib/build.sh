4b0c16e7273c32bdd6efaa06146a065fd24708ef
abexsoft <abexsoft@gmail.com>
2012-05-14 23:39:17 +0900

    added a configure option, which doesn't generate shared libraries.

--- extlib/build.sh
+++ extlib/build.sh
@@ -1,4 +1,4 @@
 
 BUILD_DIR=${1:-$PWD/build}
 
-(cd enet-1.3.3 && ./configure --prefix=$BUILD_DIR && make && make install)
+(cd enet-1.3.3 && ./configure --prefix=$BUILD_DIR --enable-shared=no && make && make install)
